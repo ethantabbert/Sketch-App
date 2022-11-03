5397668b7e947b8216ded40142fb2116aba45a88
Ethan Tabbert <etabbert@vermeer.com>
2022-11-03 14:35:36 -0500

    more stuffs

--- sketchPad.js
+++ sketchPad.js
@@ -1,6 +1,7 @@
 var canvas = document.getElementById("OMPCanvas");
-var context = canvas.getContext("2d");
-var button = document.getElementById("modeButton");
+var drawContext = canvas.getContext("2d");
+var textContext = canvas.getContext("2d");
+var textButton = document.getElementById("modeButton");
 
 var radius = 5;
 var start = 0;
@@ -9,49 +10,92 @@ var dragging = false;
 
 var toggleBool = true;
 
-button.addEventListener('click', function onClick() {
-    console.log("clicked")
+var textEntries = [];
+var textX = 0;
+var textY = 0;
+
+canvas.width = window.innerWidth - 21;
+canvas.height = window.innerHeight * .8;
+
+window.onload = drawingMode();
+
+textButton.addEventListener('click', function onClick() {
     if (toggleBool) {
-        button.style.backgroundColor = "#0a682f";
-        button.style.color = "white";
+        textButton.style.backgroundColor = "#0a682f";
+        textButton.style.color = "white";
+        textButton.textContent = "Text Mode";
         toggleBool = false;
+        textMode();
     } else {
-        button.style.backgroundColor = "#ffc425";
-        button.style.color = "black";
+        textButton.style.backgroundColor = "#ffc425";
+        textButton.style.color = "black";
+        textButton.textContent = "Draw Mode";
         toggleBool = true;
+        drawingMode();
     }
 });
 
-canvas.width = window.innerWidth - 18;
-canvas.height = window.innerHeight * .8;
+function textMode() {
+    canvas.addEventListener('mousedown', function(e) {
+        textX, textY = getCursorPosition(canvas, e);
+    })
+    console.log(textX, textY);
 
-context.lineWidth = radius * 2;
+    //textContext.fillText("hi", textX, textY);
+}
 
-var putPoint = function(e){
-    if(dragging){
-        context.lineTo(e.offsetX, e.offsetY);
-        context.strokeStyle = "white";
-        context.stroke();
-        context.beginPath();
-        context.arc(e.offsetX, e.offsetY, radius, start, end);
-        context.fillStyle = "white";
-        context.fill();
-        context.beginPath();
-        context.moveTo(e.offsetX, e.offsetY);
+function drawingMode() {
+    drawContext.lineWidth = radius * 2;
+    
+    var putPoint = function(e){
+        if(dragging){
+            drawContext.lineTo(e.offsetX, e.offsetY);
+            drawContext.strokeStyle = "white";
+            drawContext.stroke();
+            drawContext.beginPath();
+            drawContext.arc(e.offsetX, e.offsetY, radius, start, end);
+            drawContext.fillStyle = "white";
+            drawContext.fill();
+            drawContext.beginPath();
+            drawContext.moveTo(e.offsetX, e.offsetY);
+        }
+    }
+    
+    var engage = function(e){
+        dragging = true;
+        putPoint(e);
+    }
+    
+    var disengage = function(e){
+        dragging = false;
+        drawContext.beginPath();
+    }
+    
+    canvas.addEventListener('mousedown', engage);
+    canvas.addEventListener('mousemove', putPoint);
+    canvas.addEventListener('mouseup', disengage);
+
+    if (!toggleBool) {
+        return;
     }
 }
 
-var engage = function(e){
-    dragging = true;
-    putPoint(e);
+function textInput() {
+    if (toggleBool) {
+        let entry = prompt("Enter text:");
+        if (entry == null || entry == "") {
+            return;
+        } else {
+            textEntries.append(entry);
+        }
+    }
 }
 
-var disengage = function(e){
-    dragging = false;
-    context.beginPath();
+function getCursorPosition(canvas, event) {
+    const rect = canvas.getBoundingClientRect()
+    const x = event.clientX - rect.left
+    const y = event.clientY - rect.top
+    //console.log("x: " + x + " y: " + y)
+    return x, y;
 }
-
-canvas.addEventListener('mousedown', engage);
-canvas.addEventListener('mousemove', putPoint);
-canvas.addEventListener('mouseup', disengage);
 
